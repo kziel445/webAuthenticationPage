828d29356be2390a55aeb6aa14257741b25a77c0
Orzoch <47740876+kziel445@users.noreply.github.com>
2022-05-12 13:57:59 +0200

    add contact model

---
+++ WebAppContacts/WebAppContacts/Models/Contacts.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppContacts.Models
+{
+    public class Contacts
+    {
+        public enum Category
+        {
+            Private,
+            Business,
+            Other
+        }
+        [Key]
+        public Guid Id { get; init; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public Category NumberCategory { get; set; }
+        public string SubCategory { get; set; }
+        public string PhoneMuber { get; set; }
+        public DateTimeOffset BirthDate { get; set; }
+
+    }
+}
